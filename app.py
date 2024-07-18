f956b9ee759f9cacea6799e7f0cbc3bca2c3b0b2
Youstina Atef <youstinaatif43@gmail.com>
2024-07-18 16:50:20 +0300

    updates

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask, request, jsonify
-import joblib
+from flask import Flask
 
 app = Flask(__name__)
 
-# Load your model
-model = joblib.load('priority_model.pkl')
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-    # Assuming the model expects a list of features
-    prediction = model.predict([data['features']])
-    return jsonify({'priority': prediction[0]})
-
+@app.route('/')
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    from waitress import serve
-
-    serve(app, host='0.0.0.0', port=5000)
+    app.run()
